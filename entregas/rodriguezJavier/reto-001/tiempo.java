74a358c222326169bbfd3f861e88f0eca2794334
javierrodriguezrobles07-creator <javierrodriguezrobles07@gmail.com>
2026-09-20 18:52:34 +0200

    Metodo imprimir con informacion del minuto y llegada

--- entregas/rodriguezJavier/reto-001/tiempo.java
+++ entregas/rodriguezJavier/reto-001/tiempo.java
@@ -2,9 +2,11 @@ public class Tiempo {
     private int minutoActual;
     private final int MINUTOS_TOTALES = 120;
     private final int HORA_INICIO = 9;
+    private Console console;
 
     public Tiempo() {
         this.minutoActual = 0;
+        this.console = new Console();
     }
 
     public void avanzarMinuto() {
@@ -23,5 +25,14 @@ public class Tiempo {
         int hora = HORA_INICIO + (this.minutoActual / 60);
         int minutos = this.minutoActual % 60;
         return hora + ":" + minutos;
+    }
+
+    public void imprimir(boolean llegaClienteEsteMinuto) {
+        console.write(formatearHora() + " ");
+        if (llegaClienteEsteMinuto) {
+            console.writeln("llego un cliente");
+        } else {
+            console.writeln("no llego un cliente");
+        }
     }
 }
