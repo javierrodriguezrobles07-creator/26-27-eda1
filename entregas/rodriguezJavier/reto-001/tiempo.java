3f3e057c746b5f2a30c3bb67050fe3780b677b0b
javierrodriguezrobles07-creator <javierrodriguezrobles07@gmail.com>
2026-09-20 18:34:55 +0200

    Metodo para formatear la hora a partir de los minutos

--- entregas/rodriguezJavier/reto-001/tiempo.java
+++ entregas/rodriguezJavier/reto-001/tiempo.java
@@ -1,6 +1,7 @@
 public class Tiempo {
     private int minutoActual;
     private final int MINUTOS_TOTALES = 120;
+    private final int HORA_INICIO = 9;
 
     public Tiempo() {
         this.minutoActual = 0;
@@ -16,5 +17,11 @@ public class Tiempo {
 
     public int obtenerMinutoActual() {
         return this.minutoActual;
+    }
+
+    private String formatearHora() {
+        int hora = HORA_INICIO + (this.minutoActual / 60);
+        int minutos = this.minutoActual % 60;
+        return hora + ":" + minutos;
     }
 }
