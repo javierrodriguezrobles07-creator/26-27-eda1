738d377ef906d3d99f3ddb4ed83bace2e0f79cb1
javierrodriguezrobles07-creator <javierrodriguezrobles07@gmail.com>
2026-09-20 17:12:13 +0200

    Creacion de Console con metodos basicos de escritura

---
+++ entregas/rodriguezJavier/reto-001/console.java
@@ -0,0 +1,14 @@
+public class Console {
+
+    public void write(String mensaje) {
+        System.out.print(mensaje);
+    }
+
+    public void writeln(String mensaje) {
+        System.out.println(mensaje);
+    }
+
+    public void writeln() {
+        System.out.println();
+    }
+}
